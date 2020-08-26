49a6f68fedebf37fcd73cd84d13426c5489ba7fe
bobstar <quanwenbo@163.com>
2020-08-26 11:24:17 +0800

    add some

--- 笔记.rs
+++ 笔记.rs
@@ -7,9 +7,9 @@ git config --global color.ui auto
 changes:
     stash                      隐藏
     discard                    丢弃
-    stage      unstage         冻结             =   git add .          ===》 commit 
+    stage      unstage         暂存（到暂存区index）             =   git add .          ===》 commit 
 
-git commit
+git commit                     提交到当前分支
 
 
 fatal   致命问题
