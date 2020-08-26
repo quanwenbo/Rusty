d03bd7692e2111785d11085d1a5ad7d6b67c118f
bobstar <quanwenbo@163.com>
2020-08-26 11:04:21 +0800

    笔记添加咯

--- 笔记.rs
+++ 笔记.rs
@@ -1,7 +1,15 @@
+git init 
+git config --global user.name "[name]"
+git config --global user.email "[email address]"
+git config --global color.ui auto
+
+
 changes:
     stash                      隐藏
     discard                    丢弃
     stage      unstage         冻结             =   git add .          ===》 commit 
+
+git commit
 
 
 fatal   致命问题
